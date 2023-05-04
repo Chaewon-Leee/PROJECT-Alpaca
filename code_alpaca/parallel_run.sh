52776bb0a587a225c004aa00ecc6c857a28ee93c
https://github.com/Chaewon-Leee <oochaewonoo@gmail.com>
2023-05-04 23:11:03 +0900

    update : parallel run.sh

--- code_alpaca/parallel_run.sh
+++ code_alpaca/parallel_run.sh
@@ -2,17 +2,19 @@ python train.py \
   --model_name_or_path "chainyo/alpaca-lora-7b"\
   --data_path ./code_regen.json \
   --output_dir ./result \
-  --batch_size 128 \
-  --micro_batch_size 4 \
-  --num_epochs 3 \
   --learning_rate 2e-5 \
-  --cutoff_len 512 \
-  --val_set_size 2000 \
-  --lora_r 8 \
-  --lora_alpha 16 \
-  --lora_dropout 0.05 \
   --lr_scheduler_type "cosine" \
-  --lora_target_modules '[q_proj,v_proj]' \
-  --train_on_inputs \
   --group_by_length \
-  --save_steps 1000
+  --per_gpu_train_batch_size 4 \
+  --per_device_train_batch_size 4 \
+  --save_steps 1000 \
+  --num_train_epochs 3
+  # --train_on_inputs \
+  # --lora_target_modules '[q_proj,v_proj]' \
+  # --lora_dropout 0.05 \
+  # --lora_r 8 \
+  # --lora_alpha 16 \
+  # --val_set_size 2000 \
+  # --cutoff_len 512 \
+  # --batch_size 128 \
+  # --micro_batch_size 4 \
